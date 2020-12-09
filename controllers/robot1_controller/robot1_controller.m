e99761e04dca5accb36aa0aa2596f73ff5bdeb1b
RadimRadaz <74871070+RadimRadaz@users.noreply.github.com>
2020-12-09 13:22:20 +0100

    throttle_ready

--- controllers/robot1_controller/robot1_controller.m
+++ controllers/robot1_controller/robot1_controller.m
@@ -10,7 +10,7 @@
 %desktop;
 %keyboard;
 
-TIME_STEP = 64;
+TIME_STEP = 64; %ms
 
 % get and enable devices, e.g.:
 %  camera = wb_robot_get_device('camera');
@@ -24,43 +24,38 @@ wb_motor_set_position(FLwheel, Inf);
 wb_motor_set_position(FRwheel, Inf);
 wb_motor_set_position(BLwheel, Inf);
 wb_motor_set_position(BRwheel, Inf);
-throttle(1,1,1,1,3)
+
 % main loop:
 % perform simulation steps of TIME_STEP milliseconds
 % and leave the loop when Webots signals the termination
-%
+%% MAIN ACTIONS
 while wb_robot_step(TIME_STEP) ~= -1
 
+throttle(1,1,1,1)
+t = wb_robot_get_time()
+if t > 3
+  throttle(0,0,0,0)
+end
   % read the sensors, e.g.:
   %  rgb = wb_camera_get_image(camera);
   % Process here sensor data, images, etc.
-
   % send actuator commands, e.g.:
 
   % if your code plots some graphics, it needs to flushed like this:
   drawnow;
 
 end
-
-function [] = throttle(FL,FR,BL,BR, timer)
-start = wb_robot_get_time();
-endtime = 0;
+%%FUNCTIONS
+function [] = throttle(FL,FR,BL,BR)
 FLwheel = wb_robot_get_device('FLwheel');
 FRwheel = wb_robot_get_device('FRwheel');
 BLwheel = wb_robot_get_device('BLwheel');
 BRwheel = wb_robot_get_device('BRwheel');
 
-while((endtime - start) <= timer)
 wb_motor_set_velocity(FLwheel, FL);
 wb_motor_set_velocity(FRwheel, FR);
 wb_motor_set_velocity(BRwheel, BR);
 wb_motor_set_velocity(BLwheel, BL);
-endtime = wb_robot_get_time();
 end
-wb_motor_set_velocity(FLwheel, 0);
-wb_motor_set_velocity(FRwheel, 0);
-wb_motor_set_velocity(BRwheel, 0);
-wb_motor_set_velocity(BLwheel, 0);
-pause(1)
-end
+
 % cleanup code goes here: write data to files, etc.
