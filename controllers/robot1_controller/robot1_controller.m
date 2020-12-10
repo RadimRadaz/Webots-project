bc2fc7a14cfe0dca4688863cbfa1df12aaf6c6d0
RadimRadaz <74871070+RadimRadaz@users.noreply.github.com>
2020-12-10 09:55:28 +0100

    backend_adjustment

--- controllers/robot1_controller/robot1_controller.m
+++ controllers/robot1_controller/robot1_controller.m
@@ -35,19 +35,18 @@ s=43*i
     if t > 17.205+s
     vidlice_up;
     end
-     if t > 19.5+s
+    if t > 19.5+s
     throttle(-3,-3,-3,-3);
     end
-     if t > 26+s
+    if t > 26+s
     throttle(2,2,-1,-1);
     end
-     if t > 29.205+s
+    if t > 29.205+s
     throttle(3,3,3,3);
     end
     if t > 37.205+s
     vidlice_down;
     end
-
     if t> 40+s
     break
     end
@@ -65,8 +64,9 @@ while(wb_robot_step(TIME_STEP) ~= -1)
 r = wb_robot_get_time()-reset
 throttle(-5,-5,-5,-5)
 end
+
 %%FUNCTIONS
-function []=vidlice_down()
+function [] = vidlice_down()
 throttle(0,0,0,0)
 vidlice = wb_robot_get_device('vidlice');
 kloub1 = wb_robot_get_device('kloub1');
@@ -76,13 +76,12 @@ wb_motor_set_velocity(kloub1,1);
 wb_motor_set_velocity(kloub2,2);
 wb_motor_set_velocity(vidlice,2);
 
-
 wb_motor_set_position(kloub1,0);
 wb_motor_set_position(kloub2,-pi/6);
 wb_motor_set_position(vidlice,pi/3);
 end
 
-function []=vidlice_up()
+function [] = vidlice_up()
 throttle(0,0,0,0)
 vidlice = wb_robot_get_device('vidlice');
 kloub1 = wb_robot_get_device('kloub1');
@@ -114,7 +113,7 @@ wb_motor_set_velocity(BRwheel, BR);
 wb_motor_set_velocity(BLwheel, BL);
 end
 
-function [] = turn(FL, BL, FR, BR)
+function [] = turn(FL, BL, FR, BR) %optional
 FLwheel = wb_robot_get_device('FLwheel');
 FRwheel = wb_robot_get_device('FRwheel');
 BLwheel = wb_robot_get_device('BLwheel');
@@ -124,7 +123,6 @@ wb_motor_set_position(FLwheel, FL*2*pi);
 wb_motor_set_position(FRwheel, FR*2*pi);
 wb_motor_set_position(BLwheel, BL*2*pi);
 wb_motor_set_position(BRwheel, BR*2*pi);
-
 end
 
 % cleanup code goes here: write data to files, etc.
