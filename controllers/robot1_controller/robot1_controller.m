bf6a427253f3b746b7a11cc63525e0678eaf6809
RadimRadaz <74871070+RadimRadaz@users.noreply.github.com>
2020-12-13 12:33:16 +0100

    Revert "worname"
    This reverts commit a9f9a5294d47e23d62817c8806b4b022f777c31b.

--- controllers/robot1_controller/robot1_controller.m
+++ controllers/robot1_controller/robot1_controller.m
@@ -1,7 +1,7 @@
 % MATLAB controller for Webots
 % File:          robot1_controller.m
 % Date:
-% Description: BDSM
+% Description: BDSM/ Mr. Palette
 % Author: Radim Zlámal/ Petr Čagan
 % Modifications:
 
